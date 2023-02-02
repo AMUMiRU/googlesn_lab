f461808c0133d2e9d6bf37ad838a4916027b598d
AMURiN <aomaomaom1506@gmail.com>
2023-02-02 15:47:20 +0700

    goooooooooooooooogle

---
+++ lib/services/auth_service.dart
@@ -0,0 +1,15 @@
+import 'package:google_sign_in/google_sign_in.dart';
+
+class GoogleAuthService {
+  Future<void> signinWithGoogle() async {
+    try {
+      final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+      print("sussess");
+    } catch (err) {
+      print(err);
+    }
+  }
+  void googlesignout(){
+    
+  }
+}
